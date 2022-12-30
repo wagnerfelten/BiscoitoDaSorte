d853f9ad4a6038e6366ccf3ff5b8e25e253b29ea
WagnerRodrigues <felten19@gmail.com>
2022-12-30 15:50:53 -0300

    ajustado erro na animacao do btn1

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,21 @@ let card1 = document.querySelector(".card1")
 let card2 = document.querySelector(".card2")
 
 
+
+let phraser = [
+    'A vida trará coisas boas se tiver paciência.',
+    'Não compense na ira o que lhe falta na razão.',
+    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível',
+    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
+    'A juventude não é uma época da vida, é um estado de espírito',
+
+]
+
+let randomPhraser = Math.round(Math.random() * phraser.length);
+
+
 function newCard2 (event) {
     event.preventDefault();
     
@@ -10,7 +25,13 @@ function newCard2 (event) {
 
     
 
-    console.log("deu certo")
+    console.log(randomPhraser)
 }
 
 
+const reset = () => {
+    card2.classList.add("disappear");
+    card1.classList.remove("disappear");
+
+    randomPhraser = Math.round(Math.random() * phraser.length);
+}
