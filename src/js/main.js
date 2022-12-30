e29cc03f1c6dedd81f6009c1cb160f81d7a79db2
WagnerRodrigues <felten19@gmail.com>
2022-12-30 10:31:07 -0300

    resolvido problema de class que interferia no funcionamento da class .disappear{   display: none; } na linha 17

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
-let card1 = document.querySelector('.card1')
-let card2 = document.querySelector('.card2')
+let card1 = document.querySelector(".card1")
+let card2 = document.querySelector(".card2")
 
 
 function newCard2 (event) {
     event.preventDefault();
     
-    card1.classList.add("hide");
-    card2.classList.remove("hide");
+    card1.classList.add("disappear");
+    card2.classList.remove("disappear");
 
-    alert('deu certo')
+    
 
     console.log("deu certo")
 }
+
+
