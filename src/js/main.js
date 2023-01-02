2daaa9d0904277f3523c0ce441131b36aa28ca25
WagnerRodrigues <felten19@gmail.com>
2023-01-02 15:35:15 -0300

    ajustado os botao de reset, mas nao conseguido atualizar a tag p

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,56 @@
-let card1 = document.querySelector(".card1")
-let card2 = document.querySelector(".card2")
+let card1 = document.querySelector(".card1");
+let card2 = document.querySelector(".card2");
 
+let randomPhraser
 
+randomPhraser = Math.round(Math.random() * 7);
 
-let phraser = [
-    'A vida trará coisas boas se tiver paciência.',
-    'Não compense na ira o que lhe falta na razão.',
-    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
-    'A maior de todas as torres começa no solo.',
-    'Não há que ser forte. Há que ser flexível',
-    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
-    'A juventude não é uma época da vida, é um estado de espírito',
+switch (randomPhraser ) {
+  case 1:
+    document.querySelector(".card2 p").innerText =
+      "A vida trará coisas boas se tiver paciência.";
+    break;
 
-]
+  case 2:
+    document.querySelector(".phrase p").innerText =
+      "Defeitos e virtudes são apenas dois lados da mesma moeda.";
+    break;
 
-let randomPhraser = Math.round(Math.random() * phraser.length);
+  case 3:
+    document.querySelector(".phrase p").innerText =
+      "A maior de todas as torres começa no solo.";
+    break;
 
+  case 4:
+    document.querySelector(".phrase p").innerText =
+      "Não há que ser forte. Há que ser flexível";
+    break;
 
-function newCard2 (event) {
-    event.preventDefault();
-    
-    card1.classList.add("disappear");
-    card2.classList.remove("disappear");
+  case 5:
+    document.querySelector(".phrase p").innerText =
+      "Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração";
+    break;
 
-    
-
-    console.log(randomPhraser)
+  case 7:
+    document.querySelector(".phrase p").innerText =
+      "A juventude não é uma época da vida, é um estado de espírito";
+    break;
 }
 
+function newCard2(event) {
+  event.preventDefault();
+
+  card1.classList.add("disappear");
+  card2.classList.remove("disappear");
+
+ 
+  console.log(randomPhraser);
+}
 
 const reset = () => {
-    card2.classList.add("disappear");
-    card1.classList.remove("disappear");
+  card2.classList.add("disappear");
+  card1.classList.remove("disappear");
 
-    randomPhraser = Math.round(Math.random() * phraser.length);
-}
+
+  randomPhraser = Math.round(Math.random() * 6);
+};
